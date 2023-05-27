6374395afcab4045bbe7c4e4b6446b7dfc49c86f
showyha08 <over.and.uver@gmail.com>
2023-05-28 01:57:47 +0900

    mod_key

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,14 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // 環境変数が設定されていなかったらエラーを投げる
 if (!SUPABASE_URL) {
   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 }
 if (!SUPABASE_KEY) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_KEY");
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
 }
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
