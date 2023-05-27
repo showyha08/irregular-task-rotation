c44950ed3093663ad4d24fe38d00f55e91ac1b21
showyha08 <over.and.uver@gmail.com>
2023-05-27 22:56:31 +0900

    warn_resolve

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
     appDir: true,
   },
   reactStrictMode: true,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
-
